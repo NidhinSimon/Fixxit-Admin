c95d7bfbfab69a4d1183cc71bbc88abdca10c00d
NidhinSimon <n758899@gmail.com>
2023-11-18 09:18:10 +0530

    logo fix

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -86,6 +86,7 @@ navigate('/users')
     <div className="mt-4">
       <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" htmlFor="LoggingEmailAddress">Email Address</label>
       <input  value={email}
+      placeholder='Enter the Email'
               onChange={(e)=>setEmail(e.target.value)} id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="email" />
     </div>
 
@@ -96,6 +97,7 @@ navigate('/users')
       </div>
 
       <input  value={password}
+      placeholder='Enter the Password'
               onChange={(e)=>setpassword(e.target.value)} id="loggingPassword" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="password" />
     </div>
 
